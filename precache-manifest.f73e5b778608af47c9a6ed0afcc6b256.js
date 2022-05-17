ef65cae675f4d93fe78d871836dc9691adffced3
Brucezhiyu <lushuke5@gmail.com>
2022-05-17 15:03:57 +0800

    deploy

---
+++ precache-manifest.f73e5b778608af47c9a6ed0afcc6b256.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844beb4a4147404f8c1c",
+    "url": "/qianduoduo-1-website/css/app.4a604e1a.css"
+  },
+  {
+    "revision": "b2cf9f51ea44707d9e8f",
+    "url": "/qianduoduo-1-website/css/chunk-vendors.e9013b14.css"
+  },
+  {
+    "revision": "53ac44cfd4923967009b8c46d594443a",
+    "url": "/qianduoduo-1-website/erweima.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/qianduoduo-1-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/qianduoduo-1-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "163a77009e20b4b566c15277eb856b4a",
+    "url": "/qianduoduo-1-website/index.html"
+  },
+  {
+    "revision": "844beb4a4147404f8c1c",
+    "url": "/qianduoduo-1-website/js/app.ed72615b.js"
+  },
+  {
+    "revision": "b2cf9f51ea44707d9e8f",
+    "url": "/qianduoduo-1-website/js/chunk-vendors.0fae27a6.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/qianduoduo-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qianduoduo-1-website/robots.txt"
+  }
+]);
